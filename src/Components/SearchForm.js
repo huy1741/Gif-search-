e49841b51f3348eebf27901f700acf9ae7e7908d
huy1741 <huy1741@gmail.com>
2020-06-24 18:18:29 +0300

    more code

---
+++ src/Components/SearchForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class SearchForm extends Component {
+  
+  state = {
+    searchText: ''
+  }
+  
+  onSearchChange = e => {
+    this.setState({ searchText: e.target.value });
+  }
+
+
+
+  render() {  
+    return (
+      <form className="search-form" onSubmit={this.handleSubmit} >
+        <label className="is-hidden" htmlFor="search">Search</label>
+        <input type="search" 
+               onChange={this.onSearchChange}
+               name="search" 
+               ref={(input) => this.query = input}
+               placeholder="Search..." />
+        <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
+      </form>      
+    );
+  }
+}
